1840045d7a3d2201c0ae208b184ca4b7a2114b6a
aminbnd <2212@holbertonschool.com>
2021-01-29 17:20:46 +0100

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,20 @@
 #include "sort.h"
+
+/**
+ * swap_items - Swap two items in an unsorted array
+ * @a: integer, first item
+ * @b: inetegr, secon item
+ * Return: void
+ */
+void swap_items(int *a, int *b)
+{
+	int empty;
+
+	empty = *a;
+	*a = *b;
+	*b = empty;
+}
+
 /**
  * bubble_sort - Sorts an array of integer in an ascending order
  * @array: array of integers
@@ -7,22 +23,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	/*size_t iter;*/
-	int empt;
+	/*int empt;*/
 	unsigned int i, iter;
+	bool swap;
 
 	if (!array || size < 2)
 		return;
-	for(i = 0; size -1; i++)
+	for (i = 0; size - 1; i++)
 	{
-		for(iter = 0; iter < size -1; iter++)
+		swap = false;
+		for (iter = 0; iter < size - 1; iter++)
 		{
 			if (array[iter + 1] < array[iter])
 			{
-				empt = array[iter];
-				array[iter] = array[iter + 1];
-				array[iter + 1] = empt;
+				swap_items(&array[iter], &array[iter + 1]);
+				swap = true;
 				print_array(array, size);
 			}
 		}
+		if(swap == false)
+			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include<stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,24 +20,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * boolean - Defines boolean values
- * @false: 0
- * @true: 1
- */
-typedef enum boolean
-{
-	false = 0,
-	true
-} bool;
-
 
 /***** Prototypes ***/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap_items(int a, int b);
+void swap_items(int *a, int *b);
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
 
 
 #endif /*SORT_H*/
