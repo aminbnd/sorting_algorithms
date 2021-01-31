318f8405638babc70de0cc0ded89194e95b4cdb4
aminbnd <2212@holbertonschool.com>
2021-01-31 20:48:51 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,7 +27,7 @@ void bubble_sort(int *array, size_t size)
 
 	if (!array || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
