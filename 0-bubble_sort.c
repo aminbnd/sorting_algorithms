c5e3c918dc7aa6979ff1b1d1e82590fd123b1caf
aminbnd <2212@holbertonschool.com>
2021-01-31 21:09:22 +0100

    Bubble sort: inserting swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,11 +25,11 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
 
-	if (!array)
+	if (!array || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1 ; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
