6e0d7cec100fe73e793b156d22c4fd40fb6f071e
aminbnd <2212@holbertonschool.com>
2021-01-31 20:55:17 +0100

    Bubble sort: changing the swap function into bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,6 @@
 #include "sort.h"
 
 /**
- * swap - swap two integers
- * @a: first integer
- * @b: second integer
- */
-void swap(int *a, int *b)
-{
-	int empty;
-
-	empty = *a;
-	*a = *b;
-	*b = empty;
-}
-
-/**
  * bubble_sort - Sortes inetegers in an array
  * @array: array of integers
  * @size: size of the array
@@ -23,17 +9,20 @@ void swap(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
+	int empty;
 
 
 	if (!array || size < 2)
 		return;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				empty = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = empty;
 				print_array(array, size);
 			}
 		}
