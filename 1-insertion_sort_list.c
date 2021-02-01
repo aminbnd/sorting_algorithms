36244276c62816bdf8168a62365e0611159c8d2b
aminbnd <2212@holbertonschool.com>
2021-02-01 12:54:59 +0100

    Insertion sort and its big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sort integers of a list
+ * @list: pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp = NULL;
+	listint_t *ref = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	tmp = (*list)->next;
+	while (tmp)
+	{
+		for (ref = tmp; tmp->prev && (tmp->n < tmp->prev->n);)
+		{
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->prev->next = tmp->next;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			if (!tmp->prev)
+				*list = tmp;
+			else
+				tmp->prev->next = tmp;
+			print_list(*list);
+		}
+		tmp = ref->next;
+	}
+}
